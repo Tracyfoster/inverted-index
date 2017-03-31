f6412cc861172e9b250ae4932a1d8617436fb168
Tracy Ezebuike <tracy.ezebuike@andela.com>
2017-03-31 23:45:43 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,19 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const reload = browserSync.reload;
 
 // Static server
 gulp.task('browser-sync', () => {
   browserSync.init({
-    server: {
-      baseDir: './app/'
-    }
+    server: './app/'
   });
 });
 
 gulp.task('sync', ['browser-sync'], () => {
-  gulp.watch('./app/index.html', browserSync.reload);
-  gulp.watch('./app/css/*.css', browserSync.reload);
-  gulp.watch('./app/js/*.js', browserSync.reload);
+  gulp.watch('./app/index.html').on('change', reload);
+  gulp.watch('./app/css/*.css').on('change', reload);
+  gulp.watch('./app/js/*.js').on('change', reload);
 });
 
 gulp.task('browserify', () =>
